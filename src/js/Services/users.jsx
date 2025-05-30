4073d1d440655b8b68050c1009e832735f22825c
santgeek <santiago.mordz@gmail.com>
2025-05-30 15:31:35 -0500

    build: api calls for GET and POST data

---
+++ src/js/Services/users.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const BASE_URL = "https://miniapp-backend-zjnn.onrender.com";
+
+export const addUser = async (data) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/register`, data);
+        const newUser = response.data;
+        console.log(newUser);
+        return newUser;
+    } catch {
+        throw new Error("User registration failed");
+    }
+
+}
+
+export const logUser = async (data) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/login`, data);
+        const logedUser = response.data;
+        console.log(logedUser);
+        return logedUser;
+    } catch {
+        throw new Error("User login failed");
+    }
+}
+
+export const getUser = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/user`);
+        const users = response.data;
+        return users;
+    } catch {
+        throw new Error("User data unavailable");
+    }
+}
+
+export const userService = {
+    addUser,
+    logUser,
+    getUser
+}
